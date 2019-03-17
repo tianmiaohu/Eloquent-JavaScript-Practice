9cd132cf7c8c3425e3bccc1efd4a24355b8cb32e
tianmiaohu <tianmiaohu@yahoo.com>
2019-03-16 20:42:38 -0400

    Chapter 09 creation

---
+++ ch09/DateTime.js
@@ -0,0 +1,11 @@
+console.log(Date.now());
+
+function findDate(string) {
+  var dateTime = /(\d{1,2})-(\d{1,2})-(\d{4})/;
+  var match = dateTime.exec(string);
+  return new Date(Number(match[3]),
+                  Number(match[2]) - 1,
+                  Number(match[1]));
+}
+console.log(findDate("30-1-2003"));
+// → Thu Jan 30 2003 00:00:00 GMT+0100 (CET)
